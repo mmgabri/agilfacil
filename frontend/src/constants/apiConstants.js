81fda138251f83be24eae3915758ea6aef10ccc1
mmgabri <marcelomgabriel@gmail.com>
2024-08-06 18:10:52 -0300

    layout tela

--- frontend/src/constants/apiConstants.js
+++ frontend/src/constants/apiConstants.js
@@ -1,5 +1,5 @@
 //DEV
-//export const SERVER_BASE_URL = "http://localhost:9000";
+export const SERVER_BASE_URL = "http://localhost:9000";
 
 //PROD
-export const SERVER_BASE_URL = "http://54.160.193.178:9000";
+//export const SERVER_BASE_URL = "http://54.160.193.178:9000";
