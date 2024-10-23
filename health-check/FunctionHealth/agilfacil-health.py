1eff0dcb41ad06c5c69829ba1f2f647ec41741e5
mmgabri <marcelomgabriel@gmail.com>
2024-10-23 16:10:13 -0300

    Ajuste schedulle lambda

--- health-check/FunctionHealth/agilfacil-health.py
+++ health-check/FunctionHealth/agilfacil-health.py
@@ -8,10 +8,16 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)  # Níveis: DEBUG, INFO, WARNING, ERROR, CRITICAL
 
-def lambda_handler(event, context):
-    sns_topic_arn = os.getenv("SNS_TOPIC_ARN")
-    url_api_health = os.getenv("URL_API_HEALTH")
+# Configurando variáveis de ambiente
+sns_topic_arn = os.getenv("SNS_TOPIC_ARN")
+url_api_health = os.getenv("URL_API_HEALTH")
 
+# Configurando subject e message
+subject='Alert - EC2 agilfacil Health Check'
+message_api_down='Health Check com status_code diferente de 200.'
+message_exception='Health Check com Exception.'
+
+def lambda_handler(event, context):
     try:
         # Faz a requisição HTTP
         response = requests.get(url_api_health)
@@ -24,15 +30,7 @@ def lambda_handler(event, context):
             }
         else:
             logger.error(f"Serviço fora do ar. Status HTTP: {response.status_code}")
-
-            # Envia uma mensagem SNS em caso de falha
-            sns = boto3.client('sns')
-            sns.publish(
-                TopicArn=sns_topic_arn ,
-                Message='Service is DOWN on instance!',
-                Subject='EC2 agilfacil Health Check Alert'
-            )
-            logger.info("Alerta enviado via tópico sns!")
+            sendAlert(subject,message_api_down)
             return {
                 'statusCode': 500,
                 'body': json.dumps('Service is DOWN')
@@ -40,7 +38,17 @@ def lambda_handler(event, context):
 
     except Exception as e:
         logger.exception(f"Ocorreu um erro ao chamar api health check : {str(e)}")
+        sendAlert(subject,message_exception)
         return {
             'statusCode': 500,
             'body': json.dumps(f'Error: {str(e)}')
         }
+
+def sendAlert (subject,message):    
+    sns = boto3.client('sns')
+    sns.publish(
+    TopicArn=sns_topic_arn ,
+    Message=message,
+    Subject=subject
+            )
+    logger.info("Alerta enviado via tópico sns!")
