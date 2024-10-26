dcbef286e3fafc59a8ba1740119ddfa20ab828e8
mmgabri <marcelomgabriel@gmail.com>
2024-10-26 11:44:55 -0300

    log controllers finalizado

--- backend/services/controllerService.js
+++ backend/services/controllerService.js
@@ -2,11 +2,6 @@ const { v4: uuidv4 } = require('uuid');
 const { insertRoomDb, findOneRoomDb, updateRoomDb } = require('./dbService');
 const logger = require('./cloudWatchLoggerService');
 
-const sendLogger = async (type, interacao, roomId, roomName, userId, userName, moderator, vote,  statusRoom, elapsedTime, executionStatus, message ) => {
-  const logMessage = logger.createLogObject(type, interacao, roomId, roomName, userId, userName, moderator, vote,  statusRoom, elapsedTime, executionStatus, message );
-  logger.log(logMessage);
-};
-
 const createRoom = async (req, res) => {
   const start = performance.now()
   console.log("createRoom");
@@ -28,16 +23,17 @@ const createRoom = async (req, res) => {
     let newRoom = await insertRoomDb(room);
     res.status(201).json({ roomId: newRoom._id, roomName: room.roomName, userId: user.userId, userName: user.userName, moderator: user.moderator });
     const elapsedTime = (performance.now() - start).toFixed(3);
-    sendLogger('API', 'createRoom', newRoom._id, room.roomName, user.userId, user.userName, user.moderator, user.vote,  room.statusRoom, elapsedTime, 'success', 'Room created successfully.' )
+    logger.log('API', 'createRoom', newRoom._id, room.roomName, user.userId, user.userName, user.moderator, user.vote,  room.status, elapsedTime, 'success', 'Room created successfully.' )
   } catch (error) {
     const elapsedTime = (performance.now() - start).toFixed(3);
-    sendLogger('API', 'createRoom', '', room.roomName, user.userId, user.userName, user.moderator, user.vote, room.statusRoom, elapsedTime, 'failed', 'Error creating room.' )
+    logger.log('API', 'createRoom', '', room.roomName, user.userId, user.userName, user.moderator, user.vote, room.status, elapsedTime, 'failed', 'Error creating room.' )
     return res.status(500).json({ error: 'Error creating room' });
   }
 };
 
 
 const joinRoom = async (req, res) => {
+  const start = performance.now()
   console.log("joinRoom - roomId: ", req.body.roomId);
 
   let room = null;
@@ -45,6 +41,8 @@ const joinRoom = async (req, res) => {
   try {
     room = await findOneRoomDb(req.body.roomId)
   } catch (error) {
+    const elapsedTime = (performance.now() - start).toFixed(3);
+    logger.log('API', 'joinRoom', req.body.roomId, '', '', req.body.userName, false, 0, '', elapsedTime, 'failed', 'Room not found.' )
     return res.status(error.statusCode).json({ error: error.message });
   }
 
@@ -58,23 +56,34 @@ const joinRoom = async (req, res) => {
   room.users.push(user);
 
   try {
+    //throw new Error('Something went wrong!');
     let roomUpd = await updateRoomDb(room, req.body.roomId)
     res.status(201).json({ roomId: roomUpd._id, roomName: roomUpd.roomName, userId: user.userId, userName: user.userName, moderator: user.moderator });
+    const elapsedTime = (performance.now() - start).toFixed(3);
+    logger.log('API', 'joinRoom', roomUpd._id, room.roomName, user.userId, user.userName, user.moderator, user.vote,  room.status, elapsedTime, 'success', 'User join successfully.' )
   } catch (error) {
+    const elapsedTime = (performance.now() - start).toFixed(3);
+    logger.log('API', 'joinRoom', room._id, room.roomName, user.userId, user.userName, user.moderator, user.vote, room.status, elapsedTime, 'failed', 'Error join room.' )
     return res.status(400).json({ error: 'Erro ao obter sala' });
   }
 
 };
 
 const getRoom = async (req, res) => {
+  const start = performance.now()
+
   const { id } = req.params
   console.log("getRoom - roomId: ", id);
 
   try {
     const room = await findOneRoomDb(id)
-    return res.status(200).json(room);
+    res.status(200).json(room);
+    const elapsedTime = (performance.now() - start).toFixed(3);
+    logger.log('API', 'getRoom', room._id, room.roomName, '', '', '', '',  room.status, elapsedTime, 'success', 'Room get successfully.' )
   } catch (error) {
     console.log('Erro ao consultar room:', error)
+    const elapsedTime = (performance.now() - start).toFixed(3);
+    logger.log('API', 'getRoom', id, '', '', '', '', '',  '', elapsedTime, 'failed', error.message )
     return res.status(error.statusCode).json({ error: error.message });
   }
 
