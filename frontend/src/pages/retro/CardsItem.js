7fedc74fe0029a5579dc027f32c27cf5253b2f6f
mmgabri <marcelomgabriel@gmail.com>
2024-12-20 17:03:01 -0300

    evolução card - incluindo menu 3 pontinhos

--- frontend/src/pages/retro/CardsItem.js
+++ frontend/src/pages/retro/CardsItem.js
@@ -1,13 +1,45 @@
 import React, { useState, useRef } from "react";
 import styled from "styled-components";
 import { Dropdown } from "react-bootstrap";
-import { MdMoreVert, MdCheck } from "react-icons/md";
+import { MdMoreVert, MdEdit, MdCheck } from 'react-icons/md';
+import { CiTrash } from "react-icons/ci";
 import { colors } from "@atlaskit/theme";
 
 const getBackgroundColor = (isDragging, isGroupedOver) => {
   if (isDragging) return "#D8968C";
   if (isGroupedOver) return "#77DD77";
   return "#F0E68C";
+
+  //Amarelo Claro: #FFFF00
+  //Amarelo Esverdeado: #9ACD32
+  //Amarelo Biscoito: #F0E68C - bom
+  //Amarelo Limão: #FFF700
+  //Amarelo Creme: #FFF5B7
+  //Amarelo Manteiga: #F6E3B4
+  //Amarelo Charme: #F9D423
+  //Amarelo Daffodil: #FFFF31
+  //Amarelo Banana: #FFE135
+  //Amarelo Marfim: #F4F1C1
+  //Amarelo Envelhecido: #DDBB66 - bom
+  //Amarelo Charme: #F9D423
+  //Amarelo Daffodil: #FFFF31
+  //Amarelo Banana: #FFE135
+  //Amarelo Marfim: #F4F1C1
+  //Amarelo Envelhecido: #DDBB66 - bom
+
+  //Verde Pastel: #77DD77
+  //Verde Floresta Claro: #6DBE45
+
+  //Verde Pastel: #77DD77
+  //Verde Floresta Claro: #6DBE45
+  //Rosa Pastel: #F7A7C1
+  //Rosa Quartzo: #F1C6D1
+  //Rosa Flamingo: #F6A5B2
+  //Rosa Envelhecido: #D8968C
+  //Rosa Pastel: #F7A7C1
+  //Rosa Quartzo: #F1C6D1
+  //Rosa Flamingo: #F6A5B2
+  //Rosa Envelhecido: #D8968C
 };
 
 const getBorderColor = (isDragging) => (isDragging ? "black" : "transparent");
@@ -43,7 +75,7 @@ const Container = styled.a`
       ? `2px 2px 2px ${colors.N70}, -2px -2px 2px ${colors.N70}, 1px 1px 4px ${colors.N70}`
       : "none"};
   box-sizing: border-box;
-  padding: 8px;
+  padding: 6px;
   min-height: ${imageSize}px;
   margin-bottom: 8px;
   user-select: none;
@@ -52,6 +84,7 @@ const Container = styled.a`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  padding-right: 40px; /* Adiciona espaço à direita para o ícone */
 
   &:hover,
   &:active {
@@ -67,16 +100,67 @@ const Container = styled.a`
 
 const Content = styled.div`
   flex: 1;
+  white-space: pre-wrap; /* Garante que as quebras de linha sejam preservadas */
 `;
 
 const IconContainer = styled.div`
   position: absolute;
-  top: 8px;
-  right: 8px;
+
+  top: 3px;
+  right: 8px; /* Coloca o ícone no canto superior direito */
+  z-index: 1; /* Garante que o ícone fique sobre o conteúdo */
 `;
 
-function CardItem(props) {
-  const { card, isDragging, provided, style, isClone, index } = props;
+const StyledDropdownMenu = styled(Dropdown.Menu)`
+background-color: #2f2f2f;  // Cor de fundo do menu (cinza escuro)
+border-radius: 8px;
+padding: 10px;
+
+`;
+
+const StyledDropdownItem = styled(Dropdown.Item)`
+font-size: 12px;
+color: #c0c0c0;  // Cor do texto (branco para contraste)
+background-color: #2f2f2f;  // Cor de fundo do item (cinza escuro)
+border-radius: 5px;
+
+&:hover {
+  color: #3498db;  // Cor do texto ao passar o mouse (azul)
+  background-color: #444;  // Cor de fundo ao passar o mouse (cinza mais claro)
+}
+`;
+
+const StyledMdCheck = styled(MdCheck)`
+color: #10b981;
+cursor: pointer; 
+transition: color 0.2s ease;
+
+&:hover {
+  color: #3498db; // Muda a cor ao passar o mouse
+}
+`;
+
+const StyledMdMoreVert = styled(MdMoreVert)`
+color: #4F4F4F;
+cursor: pointer; 
+transition: color 0.2s ease;
+
+&:hover {
+  color: #3498db; // Muda a cor ao passar o mouse
+}
+`;
+
+
+function CardItem({
+  card,
+  isDragging,
+  provided,
+  style,
+  isClone,
+  index,
+  isGroupedOver,
+  onSave,
+}) {
   const [isEditing, setIsEditing] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [content, setContent] = useState(card.content);
@@ -88,8 +172,11 @@ function CardItem(props) {
   };
 
   const handleSave = () => {
+    console.log(content, card.id)
     setIsEditing(false);
-    // Aqui você pode implementar a lógica de salvar o conteúdo alterado.
+    if (onSave) {
+      onSave(content, card.id); // Chama a função de salvar passando o conteúdo e o id do card
+    }
   };
 
   const handleOutsideClick = (e) => {
@@ -109,7 +196,6 @@ function CardItem(props) {
     return style ? { ...provided.draggableProps.style, ...style } : provided.draggableProps.style;
   }
 
-
   return (
     <Container
       ref={provided.innerRef}
@@ -117,48 +203,51 @@ function CardItem(props) {
       {...provided.dragHandleProps}
       style={getStyle(provided, style)}
       isDragging={isDragging}
-      isGroupedOver={props.isGroupedOver}
+      isGroupedOver={isGroupedOver} // Passando isGroupedOver corretamente
       data-is-dragging={isDragging}
       data-testid={card.id}
       data-index={index}
-
     >
       {isClone && <CloneBadge>Clone</CloneBadge>}
       <Content>
         {isEditing ? (
-          <input
-            type="text"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-            onBlur={handleSave}
+          <textarea
+            value={content} // Use a variável de estado `content` para o valor do `textarea`
+            onChange={(e) => setContent(e.target.value)} // Atualiza o estado `content` conforme o usuário digita
+            onBlur={handleSave} // Salva o conteúdo quando o `textarea` perde o foco
             autoFocus
+            rows={4} // Ajuste a quantidade de linhas conforme necessário
+            style={{ width: "100%", resize: "none" }} // Desabilita o redimensionamento manual
           />
         ) : (
-          <div>{card.content}</div>
+          <div>{card.content}</div> // Exibe o conteúdo original quando não está em modo de edição
         )}
-        <IconContainer ref={menuRef}>
-          {isEditing ? (
-            <MdCheck onClick={handleSave} style={{ cursor: "pointer" }} />
-          ) : (
-            <Dropdown show={isMenuOpen} onToggle={() => setIsMenuOpen((prev) => !prev)}>
-              <Dropdown.Toggle
-                as="div"
-                id="dropdown-basic"
-                style={{ cursor: "pointer" }}
-              >
-                <MdMoreVert />
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                <Dropdown.Item onClick={handleEdit}>Editar</Dropdown.Item>
-                <Dropdown.Item onClick={() => console.log("Excluir")}>
-                  Excluir
-                </Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          )}
-        </IconContainer>
       </Content>
-
+      <IconContainer ref={menuRef}>
+        {isEditing ? (
+          <StyledMdCheck onClick={handleSave} style={{ cursor: "pointer" }} />
+        ) : (
+          <Dropdown show={isMenuOpen} onToggle={() => setIsMenuOpen((prev) => !prev)}>
+            <Dropdown.Toggle
+              as="div"
+              id="dropdown-basic"
+              style={{ cursor: "pointer" }}
+            >
+              <StyledMdMoreVert size={15} />
+            </Dropdown.Toggle>
+            <Dropdown.Menu as={StyledDropdownMenu}>
+              <StyledDropdownItem onClick={handleEdit}>
+                <MdEdit style={{ marginRight: 5 }} />
+                Editar Card
+              </StyledDropdownItem>
+              <StyledDropdownItem onClick={() => console.log("Excluir")}>
+                <CiTrash style={{ marginRight: 5 }} />
+                Excluir Card
+              </StyledDropdownItem>
+            </Dropdown.Menu>
+          </Dropdown>
+        )}
+      </IconContainer>
     </Container>
   );
 }
