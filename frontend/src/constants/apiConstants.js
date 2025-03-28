f58fb11ba183422a8d9243f86cfaabe3cceaa78e
Marcelo Gabriel <marcelomgabriel@gmail.com>
2025-03-27 21:29:41 -0300

    melhorias

--- frontend/src/constants/apiConstants.js
+++ frontend/src/constants/apiConstants.js
@@ -1,7 +1,7 @@
 //DEV
-export const SERVER_BASE_URL = "http://localhost:9000";
-export const FRONT_BASE_URL = "http://localhost:3000";
+//export const SERVER_BASE_URL = "http://localhost:9000";
+//export const FRONT_BASE_URL = "http://localhost:3000";
 
 //PROD
-//export const SERVER_BASE_URL = "https://agilfacil.com.br:9000";
-//export const FRONT_BASE_URL  = "https://agilfacil.com.br";
+export const SERVER_BASE_URL = "https://agilfacil.com.br:9000";
+export const FRONT_BASE_URL  = "https://agilfacil.com.br";
