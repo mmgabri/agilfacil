afdd84585e45482304b995ef7dba09859fac822b
mmgabri <47045061+mmgabri@users.noreply.github.com>
2025-03-30 18:12:52 -0300

    Delete frontend directory

--- frontend/src/constants/apiConstants.js
+++
@@ -1,7 +0,0 @@
-//DEV
-//export const SERVER_BASE_URL = "http://localhost:9000";
-//export const GUEST_BASE_URL = "http://localhost:3000";
-
-//PROD
-export const SERVER_BASE_URL = "https://agilfacil.com.br:9000";
-export const GUEST_BASE_URL  = "https://agilfacil.com.br";
