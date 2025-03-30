5edef0d9bb859b85fd987973c6a9c3e4b998ccf7
Marcelo Gabriel <marcelomgabriel@gmail.com>
2025-03-30 17:32:39 -0300

    Ajuste conflito

--- frontend/src/constants/apiConstants.js
+++ frontend/src/constants/apiConstants.js
@@ -1,7 +1,7 @@
 //DEV
-export const SERVER_BASE_URL = "http://localhost:9000";
-export const FRONT_BASE_URL = "http://localhost:3000";
+//export const SERVER_BASE_URL = "http://localhost:9000";
+//export const FRONT_BASE_URL = "http://localhost:3000";
 
 //PROD
-//export const SERVER_BASE_URL = "https://agilfacil.com:9000";
-//export const FRONT_BASE_URL  = "https://agilfacil.com";
+export const SERVER_BASE_URL = "https://agilfacil.com:9000";
+export const FRONT_BASE_URL  = "https://agilfacil.com";
